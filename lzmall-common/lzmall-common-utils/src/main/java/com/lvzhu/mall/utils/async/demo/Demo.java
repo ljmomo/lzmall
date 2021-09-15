2e6b238f1a8b11c2a7c24364cc0b3b2144dc244b
lvzhu <lvzhu@youzan.com>
2021-09-15 13:58:37 +0800

    添加异步查询工具2

--- lzmall-common/lzmall-common-utils/src/main/java/com/lvzhu/mall/utils/async/demo/Demo.java
+++ lzmall-common/lzmall-common-utils/src/main/java/com/lvzhu/mall/utils/async/demo/Demo.java
@@ -1,6 +1,17 @@
 package com.lvzhu.mall.utils.async.demo;
 
+import com.google.common.collect.Lists;
+
+import com.lvzhu.mall.utils.async.AsyncQueryExecutor;
+import com.lvzhu.mall.utils.async.AsyncQueryFuture;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 /**
@@ -13,10 +24,42 @@ public class Demo {
     private DemoProviderManager providerManager;
 
 
-    private List<CommonDTO> findDemo(String name,Integer userId){
+    /**
+     * 一次要查询多个数据
+     * @param name
+     * @param userId
+     * @return
+     */
+    private List<CommonDTO> findDemo(String name, Integer userId) {
         final List<CommonProvider> providers = providerManager.getProviders();
-          return   new CommonAsyncQuery(providers).doQuery(CommonQueyParam.builder().name(name).userId(userId).build());
+        return new CommonAsyncQuery(providers).doQuery(CommonQueyParam.builder().name(name).userId(userId).build());
     }
 
 
+    private List<DemoDTO> demo2(List<String> queryList) {
+        List<DemoDTO> resultList = new ArrayList<>();
+        List<AsyncQueryFuture> futures = queryList.stream().map(query -> getFuture(query)).collect(Collectors.toList());
+        AsyncQueryExecutor.executeQuery(futures);
+        futures.stream().forEach(asyncQueryFuture -> {
+            DemoDTO demoDTO = Optional.ofNullable(asyncQueryFuture.getResult()).map(a -> {
+                DemoDTO batchDTO = (DemoDTO) a;
+                return batchDTO;
+            }).orElse(null);
+            if (Objects.nonNull(demoDTO)) {
+                resultList.add(demoDTO);
+            }
+        });
+        return resultList;
+    }
+
+
+    private AsyncQueryFuture<DemoDTO> getFuture(String query) {
+        return new AsyncQueryFuture<>(() -> doFind(query));
+    }
+
+
+    private DemoDTO doFind(String query) {
+
+        return new DemoDTO(query);
+    }
 }
---
+++ lzmall-common/lzmall-common-utils/src/main/java/com/lvzhu/mall/utils/async/demo/DemoDTO.java
@@ -0,0 +1,18 @@
+package com.lvzhu.mall.utils.async.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+/**
+ * @author lvzhu.
+ * Time 2021/9/15 1:01 下午
+ * Desc 文件描述
+ */
+@Data
+@AllArgsConstructor
+public class DemoDTO {
+
+    String name;
+
+}
